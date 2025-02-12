c39086392968cb09532a9842d9f502b802239506
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-12 12:33:58 +0530

    To update date and command

---
+++ Datecommand.sh
@@ -0,0 +1,6 @@
+#Run date command and store output to shell variables
+now="$(date)"
+#now is nothing but the vraible name
+#We print it and use the variable
+#The variable names are case sensitive so now and NOW are different variable names
+echo "This is the current date and time: $now"
