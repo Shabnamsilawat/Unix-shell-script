61f33547ea1a577570cd33e36e393c524a341518
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-14 19:09:52 +0530

    Select fruits using switch statement

---
+++ switchfruits.sh
@@ -0,0 +1,18 @@
+echo "Enter a fruit"
+read fruit
+case $fruit in
+apple)
+echo "You have entered apple"
+;;
+strawberry)
+echo "You have entered strawberry"
+;;
+cherry)
+echo "You have entered cherry"
+;;
+Mango)
+echo "You have entered mango"
+;;
+*)
+echo "Invalid selection"
+esac
